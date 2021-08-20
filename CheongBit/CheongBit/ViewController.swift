768854ae1eb009a98d06a9b34a633f51e87b87d9
yangpa043 <yangpa043@gmail.com>
2021-08-20 23:41:38 +0900

    ProjectFile 생성
    자 이제 시작이야~

---
+++ CheongBit/CheongBit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheongBit
+//
+//  Created by 짜미 on 2021/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
