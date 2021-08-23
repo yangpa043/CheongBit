b94f6c672be86a665916537e320cf97bc9dbe1be
yangpa043 <yangpa043@gmail.com>
2021-08-23 12:27:26 +0900

    messageFramework

--- Sandbox/MessageFramework/ViewController.swift
+++ Sandbox/MessageFramework/ViewController.swift
@@ -8,49 +8,45 @@
 import UIKit
 import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch result {
+        case .cancelled:
+            print("celled")
+            dismiss(animated: true, completion: nil)
+        case .sent:
+            print("sent message", controller.body ?? "")
+            dismiss(animated: true, completion: nil)
+        case .failed:
+            print("failed")
+            dismiss(animated: true, completion: nil)
+        @unknown default:
+            print("unkown Error")
+            dismiss(animated: true, completion: nil)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-        @IBAction func ReportTapped(_ sender: UIButton) {
-            
-            let composeViewController = MFMessageComposeViewController()
-            
-            guard MFMessageComposeViewController.canSendText() else {
-                print("SMS services are not available")
-                return
-            }
+        
+    @IBAction func ReportTapped(_ sender: UIButton) {
+        
+        let composeViewController = MFMessageComposeViewController()
+        
+        guard MFMessageComposeViewController.canSendText() else {
+            print("SMS services are not available")
+            return
+        }
             
             composeViewController.recipients = ["01048227008"]
             composeViewController.body = "신고합니다!"
+            composeViewController.messageComposeDelegate = self
             
             present(composeViewController, animated: true, completion: nil)
-            
-            extension ViewController: MFMessageComposeViewControllerDelegate {
-                func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
-                    switch result {
-                    case .cancelled:
-                        print("celled")
-                        dismiss(animated: true, completion: nil)
-                    case .sent:
-                        print("sent message", controller.body ?? "")
-                        dismiss(animated: true, completion: nil)
-                    case .failed:
-                        print("failed")
-                        dismiss(animated: true, completion: nil)
-                    @unknown default:
-                        print("unkown Error")
-                        dismiss(animated: true, completion: nil)
-                    }
-                }
-            }
-            
-            composeViewController
-            
+        
         }
 }
 
