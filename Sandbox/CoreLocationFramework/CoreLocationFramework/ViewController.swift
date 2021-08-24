c298309bf7e1d35ecfb869254857508db704467a
yangpa043 <yangpa043@gmail.com>
2021-08-24 10:42:26 +0900

    Create locationFramework sandbox

---
+++ Sandbox/CoreLocationFramework/CoreLocationFramework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocationFramework
+//
+//  Created by 짜미 on 2021/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
