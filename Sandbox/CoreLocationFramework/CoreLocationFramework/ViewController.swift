50d0d8ccf589d7a9274bc5a4c2ee6d2ee15e6738
yangpa043 <yangpa043@gmail.com>
2021-08-24 20:12:47 +0900

    CoreLocationFramework sandbox

--- Sandbox/CoreLocationFramework/CoreLocationFramework/ViewController.swift
+++ Sandbox/CoreLocationFramework/CoreLocationFramework/ViewController.swift
@@ -8,13 +8,65 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager:CLLocationManager!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func getLocationInfo() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        } else {
+            print("에잇..이런..gps...")
+        }
+    }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations: [CLLocation]) {
+        guard let location = manager.location else {
+            return
+        }
+        let cor = location.coordinate
+        printAddressBasedOnGPS(lati: cor.latitude, longi: cor.longitude)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
+    }
+    
+    @IBAction func actionSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            locationManager.startUpdatingLocation()
+        } else {
+            locationManager.stopUpdatingLocation()
+        }
+    }
 }
 
+extension ViewController{
+    func printAddressBasedOnGPS(lati:CLLocationDegrees, longi:CLLocationDegrees) {
+        let findLocation = CLLocation(latitude: lati, longitude: longi)
+        let geocoder = CLGeocoder()
+        let locale = Locale(identifier: "Ko-kr")
+        
+        geocoder.reverseGeocodeLocation(findLocation, preferredLocale: locale, completionHandler: {(placemarks, error) in
+            
+            if let address: [CLPlacemark] = placemarks {
+                if let name: String = address.last?.name{
+                    print(name, lati, address)
+                    self.addressLabel.text = "\(name) : GPS\(lati)"
+                }
+            }
+            
+        })
+    }
+}
