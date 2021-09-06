1598ff44ad70441ae87998564c02b5f60b089232
yangpa043 <yangpa043@gmail.com>
2021-09-06 13:42:55 +0900

    화면 UI 테스트 1

--- CheongBit/CheongBit/ViewController.swift
+++ CheongBit/CheongBit/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var switchValue:Bool = false
 
+    @IBOutlet weak var micSwitch: UIButton!
+    
+    @IBOutlet weak var micImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        switchButtonUpdate()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("tap")
+    }
+    
+    @IBAction func switchChanged(_ sender: UIButton) {
+        switchButtonUpdate()
+    }
+    
+    func switchButtonUpdate() {
+        if switchValue == true{
+//            micSwitch.setTitle("on", for: .normal)
+            micSwitch.setImage(UIImage(named: "switchOn"), for: .normal)
+            micImage.image = UIImage(named: "micOn")
+            switchValue = false
+        } else {
+//            micSwitch.setTitle("off", for: .normal)
+            micSwitch.setImage(UIImage(named: "switchOff"), for: .normal)
+            micImage.image = UIImage(named: "micOff")
+            switchValue = true
+        }
+    }
 }
 
