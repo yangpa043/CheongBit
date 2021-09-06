80b1978eba1df7b91ef6a55680af352d63dd6cfd
yangpa043 <yangpa043@gmail.com>
2021-09-06 13:52:46 +0900

    브렌치 인가?

--- CheongBit/CheongBit/ViewController.swift
+++ CheongBit/CheongBit/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         switchButtonUpdate()
-        // Do any additional setup after loading the view.
     }
 
 
