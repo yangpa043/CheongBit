12042125e108f62f6488e4cc59b6df36f71a5a9f
yangpa043 <yangpa043@gmail.com>
2021-09-07 11:28:38 +0900

    메인 페이지 신고하기 버튼 구현

--- CheongBit/CheongBit/ViewControllers/1_MainViewController.swift
+++ CheongBit/CheongBit/ViewControllers/1_MainViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     
     var switchONorOFF:Bool = false
 
@@ -22,14 +23,31 @@ class MainViewController: UIViewController {
 
 // MARK: - Actions
     @IBAction func buttonTapped(_ sender: UIButton) {
-        print("tap")
+        print("locationButtonTapped")
     }
     
     @IBAction func switchChanged(_ sender: UIButton) {
         switchButtonUpdate()
     }
+    @IBAction func reportButtonTapped(_ sender: UIButton) {
+        
+        // 메시지가 안 보내졌을 때 앱을 죽지 않도록 하는 가드
+        guard MFMessageComposeViewController.canSendText() else {
+            print("SMS services are not available")
+            return
+        }
+        
+        let composeViewController = MFMessageComposeViewController()
+        
+        composeViewController.messageComposeDelegate = self
+        composeViewController.recipients = ["01048227008"]
+        composeViewController.body = "Report"
+        present(composeViewController, animated: true, completion: nil)
+    }
     
     // MARK: - Functions
+    
+    // 마이크 스위치 함수
     func switchButtonUpdate() {
         if switchONorOFF == true{
             micSwitch.setImage(UIImage(named: "switchOn"), for: .normal)
@@ -41,5 +59,25 @@ class MainViewController: UIViewController {
             switchONorOFF = true
         }
     }
+    
+    // 신고하기 버튼 변수 차단 케이스 함수
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+            switch result {
+            case .cancelled:
+                print("cancelled")
+                dismiss(animated: true, completion: nil)
+            case .sent:
+                print("sent message:", controller.body ?? "")
+                dismiss(animated: true, completion: nil)
+            case .failed:
+                print("failed")
+                dismiss(animated: true, completion: nil)
+            @unknown default:
+                print("unkown Error")
+                dismiss(animated: true, completion: nil)
+            }
+        }
+    
+    //
 }
 
