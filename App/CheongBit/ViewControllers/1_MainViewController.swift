ad8b65221dd4a57aa820a871976aaa05b176bad6
yangpa043 <yangpa043@gmail.com>
2021-09-13 18:21:19 +0900

    팟 파일 생성 및 디테일 신고 구현
    주소 셀렉 아직임

--- App/CheongBit/ViewControllers/1_MainViewController.swift
+++ App/CheongBit/ViewControllers/1_MainViewController.swift
@@ -6,9 +6,8 @@
 //
 
 import UIKit
-import MessageUI
 
-class MainViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+class MainViewController: UIViewController {
     
     var switchONorOFF:Bool = false
 
@@ -45,18 +44,6 @@ class MainViewController: UIViewController, MFMessageComposeViewControllerDelega
     // 신고버튼
     @IBAction func reportButtonTapped(_ sender: UIButton) {
         
-//        // 메시지가 안 보내졌을 때 앱을 죽지 않도록 하는 가드
-//        guard MFMessageComposeViewController.canSendText() else {
-//            print("SMS services are not available")
-//            return
-//        }
-//
-//        let composeViewController = MFMessageComposeViewController()
-//
-//        composeViewController.messageComposeDelegate = self
-//        composeViewController.recipients = ["01048227008"]
-//        composeViewController.body = "Report"
-//        present(composeViewController, animated: true, completion: nil)
     }
     
     // MARK: - Functions
@@ -75,24 +62,6 @@ class MainViewController: UIViewController, MFMessageComposeViewControllerDelega
             switchONorOFF = true
         }
     }
-    
-    // 메시지 전송 변수 차단 케이스 함수
-    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
-            switch result {
-            case .cancelled:
-                print("cancelled")
-                dismiss(animated: true, completion: nil)
-            case .sent:
-                print("sent message:", controller.body ?? "")
-                dismiss(animated: true, completion: nil)
-            case .failed:
-                print("failed")
-                dismiss(animated: true, completion: nil)
-            @unknown default:
-                print("unkown Error")
-                dismiss(animated: true, completion: nil)
-            }
-        }
-    
+        
 }
 
--- App/CheongBit/ViewControllers/ReportDetail.swift
+++ App/CheongBit/ViewControllers/ReportDetail.swift
@@ -6,24 +6,94 @@
 //
 
 import UIKit
+import DLRadioButton
+import MessageUI
 
-class ReportDetail: UIViewController {
+class ReportDetail: UIViewController, MFMessageComposeViewControllerDelegate {
+    
+    var reportContent:String = ""
 
+    //MARK: - Outlets
+    
+    @IBOutlet weak var fireReportButton: DLRadioButton!
+    @IBOutlet weak var rescueReportButton: DLRadioButton!
+    @IBOutlet weak var reportButton: reportButton!
+    
+    //MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        reportButton.isEnabled = false
+        
         // 네비 바 백버튼 텍스트 수정
         self.navigationController?.navigationBar.topItem?.title = "홈"
+        
+        // 라디오 버튼 디자인
+        fireReportButton.setTitle("화재 신고",for: .normal)
+        rescueReportButton.setTitle("구조, 구급 신고", for: .normal)
+        fireReportButton.iconSize = 23
+        rescueReportButton.iconSize = 23
+        fireReportButton.isIconOnRight = true
+        rescueReportButton.isIconOnRight = true
+        
     }
     
-    /*
-    // MARK: - Navigation
+    //MARK: - Actions
+    
+    // 신고 버튼 눌렸을 때
+    @IBAction func reportButtonTapped(_ sender: Any) {
+        
+        // 메시지가 안 보내졌을 때 앱을 죽지 않도록 하는 가드
+        guard MFMessageComposeViewController.canSendText() else {
+            print("SMS services are not available")
+            return
+        }
+        
+        if fireReportButton.isSelected {
+            reportContent = "화재 신고"
+            print("화재 신고")
+        } else if rescueReportButton.isSelected {
+            reportContent = "구조, 구급 신고"
+            print("구조, 구급 신고")
+        }
+        
+        let composeViewController = MFMessageComposeViewController()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        composeViewController.messageComposeDelegate = self
+        composeViewController.recipients = ["01048227008"]
+        composeViewController.body = reportContent
+        present(composeViewController, animated: true, completion: nil)
+        
     }
-    */
+    
+    @IBAction func radioButtonTapped(_ sender: Any) {
+        if fireReportButton.isSelected {
+            reportButton.isEnabled = true
+        } else if rescueReportButton.isSelected {
+            reportButton.isEnabled = true
+        }
+    }
+    
+    
+    // MARK: - functions
+    
+    // 메시지 전송 변수 차단 케이스 함수
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+            switch result {
+            case .cancelled:
+                print("cancelled")
+                dismiss(animated: true, completion: nil)
+            case .sent:
+                print("sent message:", controller.body ?? "")
+                dismiss(animated: true, completion: nil)
+            case .failed:
+                print("failed")
+                dismiss(animated: true, completion: nil)
+            @unknown default:
+                print("unkown Error")
+                dismiss(animated: true, completion: nil)
+            }
+        }
 
 }
