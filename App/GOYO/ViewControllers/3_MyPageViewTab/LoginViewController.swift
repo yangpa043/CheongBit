61daa5ef3294dab168a6a5647ad6af03a3c52e03
yangpa043 <yangpa043@gmail.com>
2021-12-07 12:19:07 +0900

    친구상태탭 재생성2

--- App/GOYO/ViewControllers/3_MyPageViewTab/LoginViewController.swift
+++ App/GOYO/ViewControllers/3_MyPageViewTab/LoginViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: - VC let/var
+    
+    
+    
+    // MARK: - Outlets
+    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
+    
+    // MARK: - VCLifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +28,14 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    // MARK: - Actions
+    
     @IBAction func loginButtonTapped(_ sender: Any) {
     }
+    
+    
+    // MARK: - Functions
     
     
 }
