c1a6edc39fce02e56783baaa75261655bfe7b830
yangpa043 <yangpa043@gmail.com>
2022-03-14 15:53:45 +0900

    google 로그인 구현 1

--- App/GOYO/AppDelegate.swift
+++ App/GOYO/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -35,5 +36,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
+        
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            // Handle other custom URL types.
+            return true
+        }
+        
+        // If not handled by this app, return false.
+        return false
+    }
 }
 
--- App/GOYO/ViewControllers/MyPageView/SignInViewController.swift
+++ App/GOYO/ViewControllers/MyPageView/SignInViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import GoogleSignIn
 
 class SignInViewController: UIViewController {
     
@@ -28,6 +31,31 @@ class SignInViewController: UIViewController {
     
     // MARK: - Actions
     
+    @IBAction func googleSignIn(_ sender: GIDSignInButton) {
+        // 구글 인증
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let config = GIDConfiguration(clientID: clientID)
+        
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { user, error in
+            guard error == nil else { return }
+            
+            // 인증을 해도 계정은 따로 등록을 해주어야 한다.
+            // 구글 인증 토큰 받아서 -> 사용자 정보 토큰 생성 -> 파이어베이스 인증에 등록
+            guard
+                let authentication = user?.authentication,
+                let idToken = authentication.idToken
+            else {
+                return
+            }
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+            
+            // 사용자 정보 등록
+            Auth.auth().signIn(with: credential) { _, _ in
+                print("로그인: \(String(describing: Auth.auth().currentUser?.displayName))")
+            }
+            // If sign in succeeded, display the app's main content View.
+        }
+    }
     
     
     // MARK: - Functions
