b97c9528fd05003ee73e50f24c011f4d9e0bfd28
yangpa043 <yangpa043@gmail.com>
2022-03-21 15:51:34 +0900

    firebase 관련 내용 삭제 (코드)

--- App/GOYO/AppDelegate.swift
+++ App/GOYO/AppDelegate.swift
@@ -6,21 +6,12 @@
 //
 
 import UIKit
-import Firebase
-import FirebaseCore
-import FirebaseFirestore
-import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
-        
-        let db = Firestore.firestore()
         
         return true
     }
@@ -40,18 +31,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        var handled: Bool
-        
-        handled = GIDSignIn.sharedInstance.handle(url)
-        if handled {
-            // Handle other custom URL types.
-            return true
-        }
-        
-        // If not handled by this app, return false.
-        return false
-    }
 }
 
